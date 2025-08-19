cb437be79b538629d176f309dc6766afe065e447
HatefAbedini <habedini1@student.gsu.edu>
2025-08-19 18:28:56 -0400

    Initial commit

---
+++ src/github_test1/Task1.java
@@ -0,0 +1,16 @@
+package github_test1;
+
+public class Task1
+{
+    public static void main(String[] args)
+    {
+        int a = 10; // 10
+        int b = 15; // 15
+        a = a + b; // 25
+        b = a -b; // 5
+        a = a -b;
+
+        System.out.println("b = " + b);
+        System.out.println("a = " + a);
+    }
+}
